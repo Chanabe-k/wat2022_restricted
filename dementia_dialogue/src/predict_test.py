1242b2c8ca331aa07e7eb77f2aefee9c991d8753
Chanabe-k <abe-k@ecei.tohoku.ac.jp>
2020-05-29 13:29:33 +0900

    make predict_test.py/run_predict_test.sh

--- dementia_dialogue/src/predict_test.py
+++ dementia_dialogue/src/predict_test.py
@@ -1,36 +1,127 @@
-from sklearn.metrics import precision_score, recall_score, f1_score
-import logging 
-import argparse
+import sys
+import os
+import logging
+import numpy as np
+from typing import Dict, Optional
+from dataclasses import dataclass, field
 
-# logging
-logging.basicConfig(format='[%(levelname)s] %(asctime)s : %(message)s', level=logging.DEBUG, datefmt='%Y/%m/%d %p %I:%M:%S')
-logging.info("evaluate pred_data ... ")
+import torch
+from run_classification import DataTrainingArguments, OriginalDataset, OriginalProcessor
 
-# args
-parser = argparse.ArgumentParser("evaluate predicted data with gold data")
-parser.add_argument('-pred', type=str, help="Path to predicted data")
-parser.add_argument('-gold', type=str, help="Path to gold data")
-parser.add_argument('--average', type=str, default = "macro", help="how to average when calculating each score (default : macro)")
+from transformers import AutoConfig, AutoModelForSequenceClassification, AutoTokenizer, EvalPrediction
+from transformers import (
+    HfArgumentParser,
+    Trainer,
+    TrainingArguments,
+    set_seed,
+)
 
-args = parser.parse_args()
+logger = logging.getLogger(__name__)
 
-pred_path = args.pred
-gold_path = args.gold
-average = args.average
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
 
-with open(pred_path) as f_pred, open(gold_path) as f_gold:
-    f_pred.readline()
-    pred_labels = [pred_line.strip().split('\t')[1] for pred_line in f_pred]
-    gold_labels = [gold_line.strip().split('\t')[1] for gold_line in f_gold]
+    model_name_or_path: str = field(
+        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+    )
+    fine_tuned_model_path: str = field(
+        metadata={"help": "Path to fine-tuned model path in output directory"}
+    )
+    config_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained config name or path if not the same as model_name"}
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None, metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+    )
 
-    assert len(pred_labels) == len(gold_labels), "not agreement between pred_data and gold_data"
+def main():
+    parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
 
-    precision = precision_score(gold_labels, pred_labels, average=average)
-    recall = recall_score(gold_labels, pred_labels, average=average)
-    f1_score = f1_score(gold_labels, pred_labels, average=average)
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+            # If we pass only one argument to the script and it's the path to a json file,
+            # let's parse it to get our arguments.
+            model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
-    print("precision : {}".format(precision))
-    print("recall : {}".format(recall))
-    print("f1_score : {}".format(f1_score))
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
+    )
+    logger.warning(
+        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+        training_args.local_rank,
+        training_args.device,
+        training_args.n_gpu,
+        bool(training_args.local_rank != -1),
+        training_args.fp16,
+    )
+    logger.info("Model parameters %s", training_args)
 
+    num_labels = 5
+
+    config = AutoConfig.from_pretrained(
+        model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+        num_labels=num_labels,
+        cache_dir=model_args.cache_dir,
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+    )
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_args.model_name_or_path,
+        from_tf=bool(".ckpt" in model_args.model_name_or_path),
+        config=config,
+        cache_dir=model_args.cache_dir,
+    )
+    # load fine-tuned model
+    model.load_state_dict(torch.load(model_args.fine_tuned_model_path))
     
+    train_dataset = OriginalDataset(data_args, tokenizer=tokenizer) if training_args.do_train else None
+    eval_dataset = OriginalDataset(data_args, tokenizer=tokenizer, mode="dev") if training_args.do_eval else None
+    test_dataset = OriginalDataset(data_args, tokenizer=tokenizer, mode="test")
+
+    def compute_metrics(p: EvalPrediction) -> Dict:
+        def simple_accuracy(preds, labels):
+            return (preds == labels).mean()
+        preds = np.argmax(p.predictions, axis=1)
+        return {"acc": simple_accuracy(preds, p.label_ids)}
+
+    # Initialize our Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        compute_metrics=compute_metrics
+    )
+
+    logging.info("*** Test ***")
+    test_datasets = [test_dataset]
+
+    for test_dataset in test_datasets:
+        predictions = trainer.predict(test_dataset=test_dataset).predictions
+        predictions = np.argmax(predictions, axis=1)
+
+        output_test_file = os.path.join(
+            training_args.output_dir, f"test_results.txt"
+        )
+        if trainer.is_world_master():
+            with open(output_test_file, "w") as writer:
+                logger.info("***** Test results *****")
+                writer.write("index\tprediction\n")
+                for index, item in enumerate(predictions):
+                    item = test_dataset.get_labels()[item]
+                    writer.write("%d\t%s\n" % (index, item))
+    return 0
+
+main()
