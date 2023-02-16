ae36d9048d57cfe63eccf3347a7cbee6204d4093
Chanabe-k <abe-k@tohoku.ac.jp>
2023-02-16 17:40:07 +0900

    add src

---
+++ src/evaluation/filter_out_sentences.py
@@ -0,0 +1,88 @@
+# Usage: 
+#  (en) python evaluation/filter_out_sentences.py --dic ../data/dic/test.dic.en --input ../data/sample_output.en --output ../work/filtered_sample_output.en --lang en
+#  (ja) python evaluation/filter_out_sentences.py --dic ../data/dic/test.dic.ja --input ../data/sample_output.ja --output ../work/filtered_sample_output.ja --lang ja
+
+from tqdm import tqdm
+import re
+import argparse
+
+def _lowercase(text: str):
+    return text.lower()
+
+def _remove_backslash(text: str):
+    replaced_text = text.replace('\\', '')
+    return replaced_text
+
+def _replace_specialchar(w: str):
+    specialchars = '+()'
+    for specialchar in specialchars:
+        w = w.replace(specialchar, '\\' + specialchar)
+    return w
+
+def filter_sentence_with_dic(s: str, dic: list, lang: str):
+    # HACK: remove unexpected backslash (caused by WAT submission system?)
+    s = _remove_backslash(s)
+    
+    orig_s = s
+    
+    # Check whether the sent contains all the restricted words
+    match_count = 0
+
+    for w in dic:
+        if lang == 'en':
+            for f in [_lowercase]:
+                w, s = f(w), f(s)
+    
+        w = _replace_specialchar(w)
+        
+        if re.search(w, s):
+            match_count += 1
+    
+    # Filter
+    if match_count == len(dic):
+        return orig_s
+    else:
+        return ''
+
+def get_dic(file_path: str):
+    """Load dictionary information (get dic_list)
+    input: file_path(str)"""
+    c = []
+    for l in open(file_path):
+        l = l.strip()
+        if l:
+            c.append(l)
+        else:
+            yield c
+            c = []        
+    yield c
+        
+def main():
+    parser = argparse.ArgumentParser(description='description')
+    parser.add_argument('--dic', type=str, help='Path to dictionary file')
+    parser.add_argument('--input', type=str, help='Path to Input file (model output)')
+    parser.add_argument('--output', type=str, help='Filterd output name')
+    parser.add_argument('--lang', type=str, choices = ['en', 'ja'], help='Input Language')
+    args = parser.parse_args()
+
+    dic_path = args.dic
+    input_path = args.input
+    filtered_output_path = args.output
+
+    dic_list = list(get_dic(dic_path))
+    print(f"dic length: {len(dic_list[:-1])}")
+
+    with open(filtered_output_path, 'w') as f_filtered:
+        # Exact match between dictionary and output 
+        with open(input_path) as f_out:
+            # Check file length == dictionary length
+            sents = [l.strip() for l in f_out]
+            assert len(sents) == len(dic_list[:-1]), "not match file length and dictionary length"
+            
+            # Filter out sentences not containing restricted vocab
+            for sent, dic in tqdm(zip(sents, dic_list[:-1])):
+                filtered_sent = filter_sentence_with_dic(sent, dic, args.lang)
+                f_filtered.write(filtered_sent + '\n')
+        print(f"> {filtered_output_path}")
+
+main()
---
+++ src/evaluation/re-ranking.py
@@ -0,0 +1,25 @@
+from itertools import groupby
+import pprint
+import argparse
+
+parser = argparse.ArgumentParser(description='description')
+parser.add_argument('-input', type=str, help='Path to input file', default='../../work/all_results_ja-en.txt')
+args = parser.parse_args()
+print(args.input)
+
+file_names = []
+all_scores = []
+for is_bleu, g in groupby(open(args.input), lambda line: line.startswith('BLEU')):
+    if is_bleu:
+        lines = list(g)
+        for l in lines:
+            _, scores = l.split(' = ', 1)
+            BLEU_score = float(scores.split()[0])
+            all_scores.append(BLEU_score)
+    else:
+        lines = list(g)
+        for l in lines:
+            file_names.append(l.strip())
+
+score_list = [(file, score) for file, score in zip(file_names, all_scores)]
+pprint.pprint(sorted(score_list, key=lambda t:t[1], reverse=True))
